823ec4d7705661b4a1f56adcbbdf9f183e60b349
Oscar Davies <oscar.william.davies@outlook.com>
2023-08-13 23:08:20 +0100

    Add Centered Div Component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import CenteredDiv from "./components/centeredDiv";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CenteredDiv height={50} sizerHeight={"vh"} width={50} sizerWidth={"vw"}>
+          <div className="card border-primary mb-3" style={{maxWidth: "50rem"}}>
+              <div className="card-header">Header</div>
+              <div className="card-body">
+                  <h4 className="card-title">Primary card title</h4>
+                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+          </div>
+      </CenteredDiv>
     </div>
   );
 }
